4f8b51a6f59591c2462f3ba6b92b0bfc043e0bb7
Yaning Liu <yaningliu@lbl.gov>
2016-07-19 17:45:38 -0700

    Add Word2Vec, Doc2Vec, words clustering, and bag of centeroids functionalities for sentiment analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,6 +1,6 @@
 """ This code does sentiment analysis
 
-Time-stamp: <2016-07-16 16:52:56 yaning>
+Time-stamp: <2016-07-19 16:24:50 yaning>
 
 Author: Yaning Liu
 Main used modules are nltk, beautifulsoup, scikit-learn, pandas
@@ -11,6 +11,7 @@ import numpy as np
 import os.path
 import sys
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
@@ -18,6 +19,14 @@ from sklearn.linear_model import SGDClassifier
 from sklearn import svm
 import pandas as pd
 from multiprocessing import Pool
+import logging
+from gensim.models import Word2Vec
+from sklearn.cluster import KMeans
+from itertools import repeat
+import time
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                            level=logging.INFO)
 
 class sentiment_analysis:
 
@@ -27,7 +36,7 @@ class sentiment_analysis:
         """The initializer of the sentiment_analysis class
 
         :param NLP_model: the natural language processing model, e.g.,
-        'BagOfWords', 'Word2Vec'
+        'BagOfWords', 'Word2Vec', 'BagOfCentroids'
         :param ML_method: the Maching learning methods used, e.g.,
         'RandomForest', 'LogisticRegression', 'MultinomialNB', SGDClassifier
         'SVM'
@@ -48,34 +57,19 @@ class sentiment_analysis:
         self.use_pool = use_pool
         self.pool_size = pool_size
 
-    def construct_NLP_model(self, df=None, dict_list=None):
+    def construct_NLP_model(self, dict_list, **kwargs):
         """Construct natural language processing model, assume
-        the reviews have been processed
 
-        :param df: the loaded, processed, clean data frame.
-        If data have been loaded into dataframe, then pass in df, and
-        set training_file_name and dict_list to be None.
-        Otherwise, set df and dict_list to be None and pass in
-        training_file_name, or only pass in dict_list
         :param dict_list: the list of dictionaries
         :returns: sentiment, vectorizer and train_data_features as class
         members
         :rtype: train_data_features: array of size nsamples x nfeatures
 
         """
+
         import review_processing as rp
         # get words
-        if df is not None:
-            # nitems = df.shape[0]
-            col_names = df.columns.values
-            if self.review_col_name not in col_names or \
-               self.sentiment_col_name not in col_names:
-                sys.exit('construct_NL_model: The name {0}/{1} cannot be '
-                         'found'.format(self.review_col_name,
-                                        self.sentiment_col_name))
-            review_list = df[self.review_col_name].values.tolist()
-            self.sentiment = df[self.sentiment_col_name].values
-        elif dict_list is not None:
+        if dict_list is not None:
             nitems = len(dict_list)
             col_names = list(dict_list[0].keys())
             if self.review_col_name not in col_names or \
@@ -86,6 +80,17 @@ class sentiment_analysis:
             review_list = [dic[self.review_col_name]
                            for dic in dict_list]
             self.sentiment = [dic[self.sentiment_col_name] for dic in dict_list]
+        elif 'df' in kwargs.keys():
+            df = kwargs['df']
+            # nitems = df.shape[0]
+            col_names = df.columns.values
+            if self.review_col_name not in col_names or \
+               self.sentiment_col_name not in col_names:
+                sys.exit('construct_NL_model: The name {0}/{1} cannot be '
+                         'found'.format(self.review_col_name,
+                                        self.sentiment_col_name))
+            review_list = df[self.review_col_name].values.tolist()
+            self.sentiment = df[self.sentiment_col_name].values
         else:
             if self.training_file_name is None:
                 sys.exit('construct_NLP_model: traning file name does not '
@@ -115,16 +120,28 @@ class sentiment_analysis:
                     sys.exit('construct_NLP_model: file type not supported '
                              'yet!')
 
-        # Training process of Bag of Words
+        # Training process of word model
         if self.NLP_model == 'BagOfWords':
             print('construct_NLP_model: Creating bag of words...')
             # for Bag of Words, the data should be a list of string
             review_list = [' '.join(rev) for rev in review_list]
-            self.vectorizer = CountVectorizer(analyzer='word',
-                                              tokenizer=None,
-                                              preprocessor=None,
-                                              stop_words=None,
-                                              max_features=self.maxfeature)
+            keys_in = kwargs.keys()
+            if 'vectorizer_type' in keys_in:
+                if kwargs['vectorizer_type'] == 'CountVectorizer':
+                    self.vectorizer = CountVectorizer(
+                        analyzer='word', tokenizer=None, preprocessor=None,
+                        stop_words=None, max_features=self.maxfeature)
+                elif kwargs['vectorizer_type'] == 'TfidfVectorizer':
+                    self.vectorizer = TfidVectorizer(
+                        analyzer='word', tokenizer=None, preprocessor=None,
+                        stop_words=None, max_features=self.maxfeature)
+            else:
+                print('construct_NLP_model: No vectorizer type provided for '
+                      'Word2Vec. CountVectorizer is used')
+                self.vectorizer = CountVectorizer(
+                    analyzer='word', tokenizer=None, preprocessor=None,
+                    stop_words=None, max_features=self.maxfeature)
+
             self.train_data_features = self.vectorizer.fit_transform(
                 review_list)
             self.train_data_features = self.train_data_features.toarray()
@@ -133,7 +150,64 @@ class sentiment_analysis:
             # dist = np.sum(self.train_data_features, axis=0)
             # for tag, count in zip(vocab, dist):
             #     print(count, tag)
+        elif self.NLP_model == 'Word2Vec':
+            # default values
+            self.w2v_num_features = 300    # Word vector dimensionality
+            keys_in = kwargs.keys()
+            if 'w2v_num_features' in keys_in:
+                self.w2v_num_features = kwargs['w2v_num_features']
+            else:
+                print('construct_NLP_model: No w2v_num_features provided for '
+                      'Word2Vec. Default value is used')
+            if 'w2v_model_name' in keys_in:
+                self.w2v_model = Word2Vec.load(kwargs['w2v_model_name'])
 
+                self.train_data_features = self.get_avg_feature_vecs(
+                    review_list, self.w2v_model, self.w2v_num_features,
+                    self.use_pool, self.pool_size)
+            else:
+                sys.exit('construct_NLP_model: No w2v model name provided for '
+                         'Word2Vec!')
+        elif self.NLP_model == 'BagOfCentroids':
+            keys_in = kwargs.keys()
+            if 'w2v_model_name' in keys_in:
+                self.w2v_model = Word2Vec.load(kwargs['w2v_model_name'])
+            else:
+                sys.exit('construct_NLP_model: No w2v model name provided for '
+                         'BagOfCentroids!')
+
+            if 'cluster_ratio' in keys_in:
+                cluster_ratio = kwargs['cluster_ratio']
+            else:
+                cluster_ratio = 5
+                print('construct_NLP_model: No cluster ratio provided for '
+                      'BagOfCentroids. Default value of 5 is used')
+
+            if 'cluster_method' in keys_in:
+                cluster_method = kwargs['cluster_method']
+            else:
+                cluster_method = 'KMeans'
+                print('construct_NLP_model: No cluster method provided for '
+                      'BagOfCentroids. Default value of KMeans is used')
+
+            print('Clustering words for BagOfCentroid...')
+            sys.stdout.flush()
+            t0 = time.time()
+            self.word_centroid_map = self.word_clustering(
+                self.w2v_model, cluster_ratio, cluster_method, self.pool_size
+            )
+            print('Clustering words finished. It takes {} seconds'.
+                  format(time.time()-t0))
+            sys.stdout.flush()
+
+            print('Clustering bags of centroids for BagOfCentroid...')
+            sys.stdout.flush()
+            t0 = time.time()
+            self.train_data_features = self.create_bags_of_centroids(
+                review_list, word_centroid_map, self.use_pool, self.pool_size)
+            print('Clustering bags of centroids finished. It takes {} seconds'.
+                  format(time.time()-t0))
+            sys.stdout.flush()
         else:
             sys.exit('construct_NLP_model: NLP_model type not supported yet!')
 
@@ -233,9 +307,19 @@ class sentiment_analysis:
         # for Bag of Words, the data should be a list of string
         if self.NLP_model == 'BagOfWords':
             review_list = [' '.join(rev) for rev in review_list]
-
-        test_data_features = self.vectorizer.transform(review_list)
-        test_data_features = test_data_features.toarray()
+            test_data_features = self.vectorizer.transform(review_list)
+            test_data_features = test_data_features.toarray()
+        elif self.NLP_model == 'Word2Vec':
+            test_data_features = self.get_avg_feature_vecs(review_list,
+                                                           self.w2v_model,
+                                                           self.w2v_num_features,
+                                                           self.use_pool,
+                                                           self.pool_size)
+        elif self.NLP_model == 'BagOfCentroids':
+            test_data_features = self.get_avg_feature_vecs(review_list,
+                                                           self.word_centroid_map,
+                                                           self.use_pool,
+                                                           self.pool_size)
 
         if self.ML_method == 'RandomForest':
             sentiment_pred = self.random_forest_model.predict(test_data_features)
@@ -247,5 +331,171 @@ class sentiment_analysis:
             sentiment_pred = self.SGD_model.predict(test_data_features)
         elif self.ML_method == 'SVM':
             sentiment_pred = self.SVM_model.predict(test_data_features)
+        else:
+            sys.exit('predict_ML_model: ML_method not known!')
 
         return sentiment_pred
+
+    @staticmethod
+    def get_feature_vec(wordlist, w2v_model, num_features,
+                        model_vocab=None):
+        """Function to average all of the word vectors in a given review
+
+        :param wordlist: a list of words from a single review
+        :param w2v_model: the trained word2vec model
+        :param num_features: the number of features in word2vec
+        :param model_vocab: set, the vocabulary of w2v model
+        :returns: feature_vec, the normalized (averaged) word features
+        :rtype: a 1d numpy vector of size num_features
+
+        """
+         # Pre-initialize an empty numpy array for speed
+        feature_vec = np.zeros((num_features, ), dtype='float32')
+
+        nwords = 0
+
+        # index2word is a list that contains the names of the words
+        # in the model's vocabulary. Convert it to a set for speed
+
+        if model_vocab is None:
+            index2word_set = set(w2v_model.index2word)
+        else:
+            index2word_set = model_vocab
+
+        # Loop over each word in the reviewand if it is in the model's
+        # vocabulary, add its feature vector to the total
+        for word in wordlist:
+            if word in index2word_set:
+                nwords += 1
+                feature_vec = np.add(feature_vec, w2v_model[word])
+
+        # Divide the result by the number of words to get the average
+        if nwords != 0:
+            feature_vec = np.divide(feature_vec, nwords)
+
+        return feature_vec
+
+    @staticmethod
+    def get_avg_feature_vecs(reviews, w2v_model, num_features,
+                             use_pool=False, pool_size=1):
+        """Given a set of reviews (each one a list of words), calculate
+
+        :param reviews: a list of a list of words
+        :param w2v_model: word2vec model
+        :param num_features: number of features
+        :param use_pool: bolean if use pool
+        :param pool_size: the size of the pool
+        :returns: review_feature_vecs
+        :rtype: a 2d numpy vector
+
+        """
+
+        index2word_set = set(w2v_model.index2word)
+
+        if not use_pool:
+            counter = 0
+            reviews_feature_vecs = np.zeros((len(reviews), num_features),
+                                            dtype="float32")
+
+            for review in reviews:
+                reviews_feature_vecs[counter] = sentiment_analysis.get_feature_vec(
+                    review, w2v_model, num_features, index2word_set)
+                counter += 1
+        elif use_pool:
+             pool = Pool(pool_size)
+             reviews_feature_vecs = pool.starmap(
+                 sentiment_analysis.get_feature_vec, zip(
+                     reviews, repeat(w2v_model), repeat(num_features),
+                     repeat(index2word_set)))
+             pool.close()
+
+             reviews_feature_vecs = np.asarray(reviews_feature_vecs)
+
+        return reviews_feature_vecs
+
+    @staticmethod
+    def word_clustering(w2v_model, cluster_ratio, cluster_method, pool_size=1):
+        """Clustering for word vectors
+
+        :param w2v_model: the w2v model
+        :param cluster_ratio: number of clusters = number of words//cluster_ratio
+        :param 'cluster_method': the clustering method
+        :param pool_size: number of cpu to use
+        :returns: word_centroid_map
+        :rtype: a dictionary dict(zip( model.index2word, idx ))
+
+        """
+
+        word_vectors = w2v_model.syn0
+        num_clusters = word_vectors.shape[0] // cluster_ratio
+
+        if cluster_method == 'KMeans':
+            kmeans_clustering = KMeans(n_clusters = num_clusters, n_jobs=pool_size)
+            idx = kmeans_clustering.fit_predict(word_vectors)
+        else:
+            sys.exit('word_clustering: clustering method {0} is not '
+                     'supportet yet'.format(cluster_method))
+
+        word_centroid_map = dict(zip(w2v_model.index2word, idx))
+
+        return word_centroid_map
+
+    @staticmethod
+    def create_a_bag_of_centroids(wordlist, word_centroid_map):
+        """Create a bag of centroids for a single review
+
+        :param wordlist: a list of words transformed from a single review
+        :param word_centroid_map: the map or words to centroid index
+        :returns: bag_of_centroids for a single review
+        :rtype: a 1d numpy array
+
+        """
+
+        num_centroids = max(word_centroid_map.values()) + 1
+        bag_of_centroids = np.zeros(num_centroids, dtype='float32')
+
+        for word in wordlist:
+            if word in word_centroid_map:
+                idx = word_centroid_map[word]
+                bag_of_centroids[index] += 1
+
+        return bag_of_centroids
+
+    @staticmethod
+    def create_bags_of_centroids(listwordlist, word_centroid_map,
+                                 use_pool=False, pool_size=1):
+        """Create bags of centroids for multiple reviews
+
+        :param listwordlist: a list of a list of words transformed from many
+        reviews
+        :param word_centroid_map: the map or words to centroid index
+        :param use_pool: boolean, if use pool
+        :param pool_size: the size of the pool
+        :returns: bags_of_centroids for many reviews
+        :rtype: a 2d numpy array
+
+        """
+        num_centroids = max(word_centroid_map.values()) + 1
+        num_reviews = len(listwordlist)
+        # bag_of_centroids = np.zeros(num_centroids, dtype='float32')
+
+        if not use_pool:
+            bags_of_centroids = np.zeros((num_reviews, num_centroids),
+                                        dtype='float32')
+            counter = 0
+            for wordlist in listwordlist:
+                bag_tmp = sentiment_analysis.create_a_bag_of_centroids(
+                    wordlist, word_centroid_map)
+                bags_of_centroids[counter] = bag_tmp
+                counter = counter + 1
+
+        elif use_pool:
+            pool = Pool(pool_size)
+            bags_of_centroids = pool.starmap(
+                sentiment_analysis.create_a_bag_of_centroids,
+                zip(listwordlist, repeat(word_centroid_map)))
+            pool.close()
+
+            bags_of_centroids = np.asarray(bags_of_centroids)
+
+        return bag_of_centroids
